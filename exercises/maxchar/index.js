16f72cf03a4e35f94cf67a2c49ebba42a6f80f4d
blevm <mjblevins7@gmail.com>
2018-08-13 15:06:42 -0400

    maxchar complete

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,27 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let obj = {}
+  let max = 0;
+  let maxChar = '';
+
+  str.split('').forEach(char => {
+    if (obj[char]) {
+      obj[char] += 1
+    } else {
+      obj[char] = 1
+    }
+  })
+
+  for (let char in obj) {
+    if (obj[char] > max) {
+      max = obj[char];
+      maxChar = char;
+    }
+  }
+
+  return maxChar;
+}
 
 module.exports = maxChar;
