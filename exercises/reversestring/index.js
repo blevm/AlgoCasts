f2d62537dae17b714cf006d883741e2adf6ece57
blevm <mjblevins7@gmail.com>
2018-08-13 13:22:02 -0400

    reversestring completed

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,21 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  ///// solution 1 /////
+  // return str.split('').reverse().join('');
+
+  ///// solution 2 /////
+  // let reversed = '';
+  //
+  // for (let char of str) {
+  //   reversed = char + reversed;
+  // }
+  //
+  // return reversed;
+
+  ///// solution 3 /////
+  return str.split('').reduce((rev, char) => char + rev, '')
+}
 
 module.exports = reverse;
